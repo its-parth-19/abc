e931aba45c524204056730f9c77413e3382b0f55
parth <parthbatra1908@gmail.com>
2024-11-25 21:48:08 +0530

    changes added

---
+++ Client/src/Components/EditProfile.js
@@ -0,0 +1,79 @@
+import React,{useState} from 'react'
+import './EditProfile.css'
+
+const EditProfile = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Profile Saved!\nName: ${formData.name}\nEmail: ${formData.email}`);
+      };
+  return(
+    <div className="profile-form-container">
+      <h1>Edit Profile</h1>
+      <hr className="hr-line" />
+      <form onSubmit={handleSubmit} className="profile-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Enter your name"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Confirm Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  )
+}
+
+export default EditProfile
